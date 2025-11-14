e2eebd0e2b4f32221fba5a2d4dd854d39181d5ad
Mir Ilya <mirmir2695@gmail.com>
2025-11-14 12:59:42 +0300

    vita

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Для работы на любом домене
+  base: '/', // Изменено с './' на '/'
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
